f528a54179da7e14f9cc7bd364188fbe05184ef2
Pietro Telino <pietrotelino@hotmail.com>
2022-11-14 09:05:55 -0300

    Aula 06 do curso de introdução ao Vue 2

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
-        inventory: 100,
+        inStock: true,
+        details: [
+            "80% cotton",
+            "20% polyester",
+            "Gender-neutral",
+        ],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: './assets/vmSocks-green-onWhite.jpg',
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: './assets/vmSocks-blue-onWhite.jpg',
+            }
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart +=1 ;
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        }
     }
 })
